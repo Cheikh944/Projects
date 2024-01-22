48e41c43ef9557d11fbd4b0e2d29b24ae28603ac
Cheikh944 <cheikhdiag@yahoo.com>
2024-01-22 11:29:19 +0100

    Second commit

--- gaming-page/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Navbar} from './navbar/Navbar';
-export {default as Home} from './home/Home';
-export {default as Tournaments} from './tournaments/Tournaments';
-export {default as Contact} from './contact/Contact';
-export {default as Shop} from './shop/Shop';
-export {default as News} from './news/News';
-export {default as Footer} from './footer/Footer';
